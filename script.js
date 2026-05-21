1b578af0202976b9350b464874e0f73b5947b775
katyperrylover <franco.larenaspiatta18@gmail.com>
2026-05-21 19:19:45 -0300

    js mejorado

--- script.js
+++ script.js
@@ -1,39 +1,56 @@
-document.getElementsById('tasaInt')[0].oninput = function () {
-    let max = parseInt(this.max);
-    let min = parseInt(this.min);
-    if (parseInt(this.value) > max) {
-        this.value = max; 
-    } else if(parseInt(this.value) < min){
-        this.value = min
+// encontre este fragmento de codigo que evita q te pases de 100% o por debajo de -1%
+function cambiarValorAlComun(input) {
+    const max = parseInt(input.max);
+    const min = parseInt(input.min);
+    const value = parseInt(input.value);
+
+    if (value > max) {
+        input.value = max;
+    } else if (value < min) {
+        input.value = min;
     }
 }
 
+document.getElementsByClassName('typeboxTasa')[0].oninput = function() {
+    cambiarValorAlComun(this);
+};
+
+document.getElementsByClassName('typeboxFecha')[0].oninput = function() {
+    cambiarValorAlComun(this);
+};
+
+// aca como q agarre al formulario y me fijo si los datos se enviaron o algo asi
 document.getElementById('formulario').onsubmit = function() {
     let resultado_texto = document.getElementById("resultado");
-
+    let resultado, temp;
+    // dinero calculado
     let dinero = Number(document.getElementById("dineroPrin").value);
+
     // tasa calculada
     let tasa = Number(document.getElementById("tasaInt").value);
-    if (tasa > 100){tasa = 100;}
-    else if(tasa < 1){tasa = 1;}
     tasa = tasa / 100;
-    let fecha = Number(document.getElementById("fechaFinal").value);
-    let resultado, temp;
 
-    if (fecha <= 2026) {
-        resultado_texto.innerText = "Error de fecha";
+    // fecha calculada
+    let fecha = Number(document.getElementById("fechaFinal").value);
+    
+    if (fecha === "" || tasa === "" || dinero === "") {
+        resultado_texto.innerText = "Error, alguna casilla esta vacia lpm";
         return false;
     }
-    
-    let anios = fecha - 2026;
+
+    //if (fecha <= 2026) {
+    //    resultado_texto.innerText = "Error de fecha";
+    //    return false;
+    //}
     let contador = 1;
 
-    while (anios > 0) {
+    // este es el bucle bien piola donde calculo el interes por el anio
+    while (fecha > 0) {
         resultado = dinero * (1+tasa);
         console.log("año " + contador + ": $" + resultado);
         dinero = resultado;
         contador++;
-        anios--;
+        fecha--;
     }
     
     resultado_texto.innerText = resultado;
