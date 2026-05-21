96685658d3d52cb0d05005f2f81db462f850e59a
katyperrylover <franco.larenaspiatta18@gmail.com>
2026-05-21 20:32:51 -0300

    miss

--- script.js
+++ script.js
@@ -22,6 +22,12 @@ function cambiarValorAlComunFecha(input) {
     document.getElementById("m").innerHTML = "Meses";
 }
 
+// esta funcion hace q los numeros tengan un formato con puntitos
+function formatoEntendible(x) {
+  return x.toLocaleString("es-AR");
+}
+
+
 // aplico la funcion para q no se pase
 document.getElementsByClassName('typeboxTasa')[0].oninput = function() {
     cambiarValorAlComun(this);
@@ -32,23 +38,26 @@ document.getElementsByClassName('typeboxFecha')[0].oninput = function() {
     cambiarValorAlComunFecha(this);
 };
 
-// esta funcion hace q los numeros tengan un formato con puntitos
-function formatoEntendible(x) {
-  return x.toLocaleString("es-AR");
-}
 
 // aca como q agarre al formulario y me fijo si los datos se enviaron o algo asi
 document.getElementById('formulario').onsubmit = function() {
     // si se enviaron empieza toda esta funcionn :)
     let resultado_texto = document.getElementById("resultado");
-    let resultado;
+    let resultado, opcionPlazo;
 
     // dinero calculado
     let dinero = Number(document.getElementById("dineroPrin").value);
 
     // tasa calculada
     let tasa = Number(document.getElementById("tasaInt").value);
-    tasa = tasa / 100;
+    if (document.getElementById("listaOpciones").value === "m") {
+        tasa = (tasa / 100) / 12;
+        opcionPlazo = "mes";
+    }
+    else{
+        tasa = tasa / 100;
+        opcionPlazo = "año";
+    }
 
     // fecha calculada
     let fecha = Number(document.getElementById("fechaFinal").value);
@@ -61,19 +70,27 @@ document.getElementById('formulario').onsubmit = function() {
 
     // el contador es importante para q la formula sea correcta
     let contador = 1;
-
-    // este es el bucle bien piola donde calculo el interes por el anio
     
+
+
+    // si la fecha es uno, se saltea el bucle
+    if (fecha === 1) {
+        resultado = parseFloat((dinero * (1+tasa)**contador).toFixed(2));
+        resultado_texto.innerText = "$ " + formatoEntendible(resultado);
+        return true;
+    }
+
+    
+    // este es el bucle bien piola donde calculo el interes por el anio
     while (fecha > 1) {
         // tuve un problema con la formula y la solucion era cambiar
         // fecha > 0 por fecha > 1 jajajajs
         resultado = parseFloat((dinero * (1+tasa)**contador).toFixed(2));
-        console.log("año " + contador + ": $" + resultado);
+        console.log(opcionPlazo + " " + contador + ": $" + resultado);
         contador++;
         fecha--;
     }
-    
-    resultado = parseFloat(resultado.toFixed(2));
+
     resultado_texto.innerText = "$ " + formatoEntendible(resultado);
     return true;
     
